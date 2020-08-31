f24184a62c1aa9b357269833b26cfd70fe7bd27f
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-08-31 18:35:11 +0530

    Updated main file.

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_ecommerce/models/app_state.dart';
 import 'package:flutter_ecommerce/pages/login_page.dart';
 import 'package:flutter_ecommerce/pages/products_page.dart';
 import 'package:flutter_ecommerce/pages/register_page.dart';
+import 'package:flutter_ecommerce/redux/reducers.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+import 'package:redux/redux.dart';
+import 'package:redux_thunk/redux_thunk.dart';
 
 void main() {
-  runApp(MyApp());
+  final store = Store<AppState>(
+    appReducer,
+    initialState: AppState.initial(),
+    middleware: [thunkMiddleware],
+  );
+  runApp(MyApp(store: store));
 }
 
 class MyApp extends StatelessWidget {
+  final Store<AppState> store;
+
+  MyApp({
+    this.store,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter E-Commerce',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        brightness: Brightness.dark,
-        primaryColor: Colors.cyan[400],
-        accentColor: Colors.deepOrange[200],
-        textTheme: TextTheme(
-          headline1: TextStyle(
-            fontSize: 72.0,
-            fontWeight: FontWeight.bold,
+    return StoreProvider(
+      store: store,
+      child: MaterialApp(
+        title: 'Flutter E-Commerce',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.cyan[400],
+          accentColor: Colors.deepOrange[200],
+          textTheme: TextTheme(
+            headline1: TextStyle(
+              fontSize: 72.0,
+              fontWeight: FontWeight.bold,
+            ),
+            // subtitle1: TextStyle(
+            //   fontSize: 36.0,
+            //   fontStyle: FontStyle.italic,
+            // ),
+            // bodyText1: TextStyle(
+            //   fontSize: 18.0,
+            // ),
           ),
-          // subtitle1: TextStyle(
-          //   fontSize: 36.0,
-          //   fontStyle: FontStyle.italic,
-          // ),
-          // bodyText1: TextStyle(
-          //   fontSize: 18.0,
-          // ),
         ),
+        home: ProductsPage(),
+        routes: {
+          ProductsPage.id: (context) => ProductsPage(),
+          LoginPage.id: (context) => LoginPage(),
+          RegisterPage.id: (context) => RegisterPage(),
+        },
       ),
-      home: ProductsPage(),
-      routes: {
-        ProductsPage.id: (context) => ProductsPage(),
-        LoginPage.id: (context) => LoginPage(),
-        RegisterPage.id: (context) => RegisterPage(),
-      },
     );
   }
 }
