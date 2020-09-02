9a28b64146a1cf3bc4036f87f8bdab8d3c423662
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-02 22:12:38 +0530

    Updated actions.

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -3,6 +3,7 @@
 import 'dart:convert';
 
 import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/models/users.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 import 'package:redux/redux.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -10,16 +11,17 @@ import 'package:shared_preferences/shared_preferences.dart';
 ThunkAction<AppState> getUserAction = (Store<AppState> store) async {
   final SharedPreferences prefs = await SharedPreferences.getInstance();
   String storedUser = prefs.getString('user');
-  final user = storedUser != null ? json.decode(storedUser) : null;
+  final User user =
+      storedUser != null ? User.fromJson(json.decode(storedUser)) : null;
   store.dispatch(GetUserAction(
     user,
   ));
 };
 
 class GetUserAction {
-  final dynamic _user;
+  final User _user;
 
-  dynamic get user => this._user;
+  User get user => this._user;
 
   GetUserAction(
     this._user,
