f62962d28c95f48f3ac22f2b5a4f145da932538f
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-03 23:33:37 +0530

    Updated the app state for products data.

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -1,16 +1,21 @@
+import 'package:flutter_ecommerce/models/product.dart';
+import 'package:flutter_ecommerce/models/users.dart';
 import 'package:meta/meta.dart';
 
 @immutable
 class AppState {
-  final dynamic user;
+  final User user;
+  final List<Product> products;
 
   AppState({
     @required this.user,
+    @required this.products,
   });
 
   factory AppState.initial() {
     return AppState(
       user: null,
+      products: [],
     );
   }
 }
