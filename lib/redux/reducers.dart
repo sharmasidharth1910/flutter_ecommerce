36d0f9b71de35bd111998fa754513128ab14a527
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-04 22:22:38 +0530

    Updated the reducers file.

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -1,10 +1,12 @@
 import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/models/product.dart';
 import 'package:flutter_ecommerce/models/users.dart';
 import 'package:flutter_ecommerce/redux/actions.dart';
 
 AppState appReducer(AppState state, dynamic action) {
   return AppState(
     user: userReducer(state.user, action),
+    products: productsReducer(state.products, action),
   );
 }
 
@@ -12,6 +14,12 @@ User userReducer(User user, dynamic action) {
   if (action is GetUserAction) {
     return action.user;
   }
-
   return user;
+}
+
+List<Product> productsReducer(List<Product> products, dynamic action) {
+  if (action is GetProductsAction) {
+    return action.products;
+  }
+  return products;
 }
