783019cf12f657e21fa94bff5a2c222a3765d82d
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-04 22:23:11 +0530

    Finalised the products page UI.

--- lib/pages/products_page.dart
+++ lib/pages/products_page.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/app_state.dart';
+import 'package:flutter_ecommerce/widgets/product_item.dart';
 import 'package:flutter_redux/flutter_redux.dart';
+
+final gradientBackground = BoxDecoration(
+  gradient: LinearGradient(
+    begin: Alignment.bottomLeft,
+    end: Alignment.topRight,
+    stops: [
+      0.1,
+      0.3,
+      0.5,
+      0.7,
+      0.9,
+    ],
+    colors: [
+      Colors.deepOrange[300],
+      Colors.deepOrange[400],
+      Colors.deepOrange[500],
+      Colors.deepOrange[600],
+      Colors.deepOrange[700],
+    ],
+  ),
+);
 
 class ProductsPage extends StatefulWidget {
   static const String id = "ProductsPage";
@@ -21,32 +43,79 @@ class _ProductsPageState extends State<ProductsPage> {
     widget.onInit();
   }
 
-  AppBar _appBar() {
-    return AppBar(
-      title: Text("Products"),
-      centerTitle: true,
-    );
-  }
+  final _appBar = PreferredSize(
+    preferredSize: Size.fromHeight(60.0),
+    child: StoreConnector<AppState, AppState>(
+      builder: (context, state) {
+        return AppBar(
+          title: SizedBox(
+            child: state.user != null ? Text(state.user.username) : Text(""),
+          ),
+          centerTitle: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.store,
+            ),
+            onPressed: () {},
+          ),
+          actions: [
+            state.user != null
+                ? IconButton(
+                    icon: Icon(
+                      Icons.exit_to_app,
+                    ),
+                    onPressed: () {},
+                  )
+                : Text(''),
+          ],
+        );
+      },
+      converter: (store) => store.state,
+    ),
+  );
 
   @override
   Widget build(BuildContext context) {
+    final Orientation orientation = MediaQuery.of(context).orientation;
+
     return StoreConnector<AppState, AppState>(
       converter: (store) => store.state,
       builder: (context, state) {
         return Scaffold(
-          appBar: _appBar(),
-          body: SingleChildScrollView(
-            child: state.user != null
-                ? Text(
-                    state.user.username +
-                        "  " +
-                        state.user.jwt +
-                        "  " +
-                        state.user.email,
-                  )
-                : Text(
-                    "No Data",
-                  ),
+          appBar: _appBar,
+          body: Container(
+            decoration: gradientBackground,
+            child: StoreConnector<AppState, AppState>(
+              builder: (context, state) {
+                return Column(
+                  children: [
+                    Expanded(
+                      child: SafeArea(
+                        top: false,
+                        bottom: false,
+                        child: GridView.builder(
+                          physics: BouncingScrollPhysics(),
+                          itemCount: state.products.length,
+                          gridDelegate:
+                              SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount:
+                                orientation == Orientation.portrait ? 2 : 3,
+                            crossAxisSpacing: 4.0,
+                            mainAxisSpacing: 4.0,
+                            childAspectRatio:
+                                orientation == Orientation.portrait ? 1.0 : 1.3,
+                          ),
+                          itemBuilder: (context, index) => ProductItem(
+                            item: state.products[index],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                );
+              },
+              converter: (store) => store.state,
+            ),
           ),
         );
       },
