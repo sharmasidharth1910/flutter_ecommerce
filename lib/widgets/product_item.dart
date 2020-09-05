05c2ac36f4da0ce37a980253b9551732b667ce1c
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-05 17:53:19 +0530

    Added custom image transitions.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ecommerce/models/app_state.dart';
 import 'package:flutter_ecommerce/models/product.dart';
+import 'package:flutter_ecommerce/pages/products_detail_page.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 class ProductItem extends StatelessWidget {
@@ -14,41 +15,54 @@ class ProductItem extends StatelessWidget {
   Widget build(BuildContext context) {
     final String pictureUrl = "http://10.0.2.2:1337${item.picture['url']}";
 
-    return GridTile(
-      footer: GridTileBar(
-        title: FittedBox(
-          fit: BoxFit.scaleDown,
-          alignment: Alignment.centerLeft,
-          child: Text(
-            item.name,
-            style: TextStyle(
-              fontSize: 20.0,
-            ),
+    return InkWell(
+      onTap: () => Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => ProductsDetailPage(
+            item: item,
           ),
-        ),
-        subtitle: Text(
-          "\$" + item.price.toString(),
-          style: TextStyle(
-            fontSize: 16.0,
-          ),
-        ),
-        backgroundColor: Color(0xBB000000),
-        trailing: StoreConnector<AppState, AppState>(
-          builder: (_, state) {
-            return state.user != null
-                ? IconButton(
-                    icon: Icon(Icons.shopping_cart),
-                    color: Colors.white,
-                    onPressed: () {},
-                  )
-                : Text("");
-          },
-          converter: (store) => store.state,
         ),
       ),
-      child: Image.network(
-        pictureUrl,
-        fit: BoxFit.cover,
+      child: GridTile(
+        footer: GridTileBar(
+          title: FittedBox(
+            fit: BoxFit.scaleDown,
+            alignment: Alignment.centerLeft,
+            child: Text(
+              item.name,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+            ),
+          ),
+          subtitle: Text(
+            "\$" + item.price.toString(),
+            style: TextStyle(
+              fontSize: 16.0,
+            ),
+          ),
+          backgroundColor: Color(0xBB000000),
+          trailing: StoreConnector<AppState, AppState>(
+            builder: (_, state) {
+              return state.user != null
+                  ? IconButton(
+                      icon: Icon(Icons.shopping_cart),
+                      color: Colors.white,
+                      onPressed: () {},
+                    )
+                  : Text("");
+            },
+            converter: (store) => store.state,
+          ),
+        ),
+        child: Hero(
+          tag: item,
+          child: Image.network(
+            pictureUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
       ),
     );
   }
