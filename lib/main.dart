cadcdaa88375dd94429ea6eb35fbfa2a230a88fb
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-05 17:52:36 +0530

    Added redux logging.

--- lib/main.dart
+++ lib/main.dart
@@ -7,13 +7,17 @@ import 'package:flutter_ecommerce/redux/actions.dart';
 import 'package:flutter_ecommerce/redux/reducers.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:redux/redux.dart';
+import 'package:redux_logging/redux_logging.dart';
 import 'package:redux_thunk/redux_thunk.dart';
 
 void main() {
   final store = Store<AppState>(
     appReducer,
     initialState: AppState.initial(),
-    middleware: [thunkMiddleware],
+    middleware: [
+      thunkMiddleware,
+      LoggingMiddleware.printer(),
+    ],
   );
   runApp(MyApp(store: store));
 }
@@ -41,13 +45,13 @@ class MyApp extends StatelessWidget {
               fontSize: 72.0,
               fontWeight: FontWeight.bold,
             ),
-            // subtitle1: TextStyle(
-            //   fontSize: 36.0,
-            //   fontStyle: FontStyle.italic,
-            // ),
-            // bodyText1: TextStyle(
-            //   fontSize: 18.0,
-            // ),
+            subtitle1: TextStyle(
+              fontSize: 36.0,
+              fontStyle: FontStyle.italic,
+            ),
+            bodyText1: TextStyle(
+              fontSize: 18.0,
+            ),
           ),
         ),
         home: LoginPage(),
@@ -56,6 +60,8 @@ class MyApp extends StatelessWidget {
                 onInit: () {
                   // dispatch an action (UserAction) to grab user data
                   StoreProvider.of<AppState>(context).dispatch(getUserAction);
+                  StoreProvider.of<AppState>(context)
+                      .dispatch(getProductsAction);
                 },
               ),
           LoginPage.id: (context) => LoginPage(),
