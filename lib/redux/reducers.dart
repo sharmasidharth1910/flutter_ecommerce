3b9e6d22812c0b59a84c67ea0e19a6d955df2859
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-06 22:10:29 +0530

    Updated the reducers.

--- lib/redux/reducers.dart
+++ lib/redux/reducers.dart
@@ -7,11 +7,14 @@ AppState appReducer(AppState state, dynamic action) {
   return AppState(
     user: userReducer(state.user, action),
     products: productsReducer(state.products, action),
+    cartProducts: cartProducts(state.cartProducts, action),
   );
 }
 
 User userReducer(User user, dynamic action) {
   if (action is GetUserAction) {
+    return action.user;
+  } else if (action is LogoutUserAction) {
     return action.user;
   }
   return user;
@@ -22,4 +25,11 @@ List<Product> productsReducer(List<Product> products, dynamic action) {
     return action.products;
   }
   return products;
+}
+
+List<Product> cartProducts(List<Product> cartProducts, dynamic action) {
+  if (action is ToggleCartProductAction) {
+    return action.cartProducts;
+  }
+  return cartProducts;
 }
