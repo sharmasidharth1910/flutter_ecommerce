e9ce7699f5d298b4cdb703fbe9543fbcdad47edf
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-07 18:33:17 +0530

    Added the logout action.

--- lib/redux/actions.dart
+++ lib/redux/actions.dart
@@ -18,6 +18,23 @@ ThunkAction<AppState> getUserAction = (Store<AppState> store) async {
   ));
 };
 
+ThunkAction<AppState> logoutUserAction = (Store<AppState> store) async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  await prefs.remove('user');
+  User user;
+  store.dispatch(LogoutUserAction(user));
+};
+
+class LogoutUserAction {
+  final User _user;
+
+  User get user => this._user;
+
+  LogoutUserAction(
+    this._user,
+  );
+}
+
 class GetUserAction {
   final User _user;
 
@@ -46,4 +63,30 @@ class GetProductsAction {
   GetProductsAction(this._products);
 
   List<Product> get products => this._products;
+}
+
+// Cart Products Actions
+
+ThunkAction<AppState> toggleCartProductAction(Product cartProduct) {
+  return (Store<AppState> store) {
+    final List<Product> cartProducts = store.state.cartProducts;
+    final int index =
+        cartProducts.indexWhere((element) => element.id == cartProduct.id);
+    bool isInCart = index > -1 == true;
+    List<Product> updatedCartProducts = List.from(cartProducts);
+    if (isInCart) {
+      updatedCartProducts.removeAt(index);
+    } else {
+      updatedCartProducts.add(cartProduct);
+    }
+    store.dispatch(ToggleCartProductAction(updatedCartProducts));
+  };
+}
+
+class ToggleCartProductAction {
+  final List<Product> _cartProducts;
+
+  List<Product> get cartProducts => this._cartProducts;
+
+  ToggleCartProductAction(this._cartProducts);
 }
