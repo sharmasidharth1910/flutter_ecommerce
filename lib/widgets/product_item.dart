9ba1a8b7c0f89e179e736317ae30581244fdbb6b
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-09 16:17:39 +0530

    Updated product item model.

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -12,6 +12,11 @@ class ProductItem extends StatelessWidget {
     @required this.item,
   });
 
+  bool _isInCart(AppState state, String id) {
+    final List<Product> cartProducts = state.cartProducts;
+    return cartProducts.indexWhere((element) => element.id == id) > -1;
+  }
+
   @override
   Widget build(BuildContext context) {
     final String pictureUrl = "http://10.0.2.2:1337${item.picture['url']}";
@@ -49,10 +54,13 @@ class ProductItem extends StatelessWidget {
               return state.user != null
                   ? IconButton(
                       icon: Icon(Icons.shopping_cart),
-                      color: Colors.white,
+                      color: _isInCart(state, item.id)
+                          ? Colors.cyan[700]
+                          : Colors.white,
                       onPressed: () {
-                        StoreProvider.of<AppState>(context)
-                            .dispatch(toggleCartProductAction(item));
+                        StoreProvider.of<AppState>(context).dispatch(
+                          toggleCartProductAction(item),
+                        );
                       },
                     )
                   : Text("");
