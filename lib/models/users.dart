e882f05dcab894f26892ea262ac8280049bed620
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-10 19:46:07 +0530

    Updated user model.

--- lib/models/users.dart
+++ lib/models/users.dart
@@ -5,17 +5,20 @@ class User {
   String username;
   String id;
   String jwt;
+  String cartId;
 
   User({
     @required this.email,
     @required this.id,
     @required this.jwt,
     @required this.username,
+    @required this.cartId,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
+      cartId: json['cart_id'],
       username: json['username'],
       email: json['email'],
       jwt: json['jwt'],
