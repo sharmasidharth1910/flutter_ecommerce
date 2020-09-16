d76a9de46f6a8a24ae6f9bce2ac0fc53a3c738a7
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-16 08:58:38 +0530

    Some minor chnages in the orders route.

--- server/api/order/controllers/order.js
+++ server/api/order/controllers/order.js
@@ -1,8 +1,15 @@
-'use strict';
+"use strict";
+
+const stripe = require("stripe")(
+  "sk_test_51HQQNzAHq1q1hh4yKWHe7O3aw8R2oZGAmgC3qJdaFeHPOgdZ4K2PLihXV2h4PDzJVxbovkajcVJw1ZkbvdOI8XJ300vNw0Vaxe"
+);
+const uuid = require("uuid/v4");
 
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  lifecycles: {},
+};
