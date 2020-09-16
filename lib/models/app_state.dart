23d65054abd358c1e144c8f5b0db73fbb10a7585
sharmasidharth1910 <sharmasidharth1910@gmail.com>
2020-09-16 09:00:07 +0530

    Updated the app state model.

--- lib/models/app_state.dart
+++ lib/models/app_state.dart
@@ -7,11 +7,15 @@ class AppState {
   final User user;
   final List<Product> products;
   final List<Product> cartProducts;
+  final List<dynamic> cards;
+  final String cardToken;
 
   AppState({
     @required this.user,
     @required this.products,
     @required this.cartProducts,
+    @required this.cards,
+    @required this.cardToken,
   });
 
   factory AppState.initial() {
@@ -19,6 +23,8 @@ class AppState {
       user: null,
       products: [],
       cartProducts: [],
+      cards: [],
+      cardToken: "",
     );
   }
 }
